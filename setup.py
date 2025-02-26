29b54b047a30464a237ea5844f569145850abb48
AasaSingh05 <aasasingh2005@gmail.com>
2025-02-26 19:59:55 +0530

    fix: added the new version number and improved setup

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="SECEdgar-Python",
-    version="0.0.2",
-    description="A simple library to interact with and retrieve information from the SEC Edgar Data.",
+    name='SECEdgar-Python',
+    version='0.1.1',
+    author='Aasa Singh Bhui',
+    author_email='aasasingh2005@gmail.com',
+    description='A Python package for SEC Edgar data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/AasaSingh05/SECEdgarPy',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     install_requires=[
-        'os>=2.1.4',
-        'pandas>=2.2.3',
-        'requests>=2.32.3',
-        'bs4>=0.0.2'
-    ]
+        'requests>=2.25.1',
+        'pandas>=1.2.0',
+        'beautifulsoup4>=4.9.3',
+        'lxml>=4.6.2',
+    ],
 )
